5d02484ca8d22affb01c7cc49f29abe884edfdf2
Philippe Cossette <philcossette@hotmail.com>
2025-09-12 13:20:27 -0400

    Added Animation on navbar

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,25 @@
 /*!
-* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+ */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function navBarState() {
+    const navBar = document.getElementById("mainNav");
+
+    if (!navBar) return;
+
+    document.addEventListener("scroll", () => {
+        if (window.scrollY > 50) {
+            navBar.classList.remove("bg-transparent");
+            navBar.classList.add("scrolled");
+        } else {
+            navBar.classList.remove("scrolled");
+            navBar.classList.add("bg-transparent");
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", navBarState);
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -18,16 +18,16 @@
 <body class="d-flex flex-column h-100">
     <main class="flex-shrink-0">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <nav id="mainNav" class="navbar navbar-expand-lg navbar-light bg-white fixed-top bg-transparent">
             <div class="container px-5">
-                <a class="navbar-brand" href="{{ url('/home') }}"><img style="max-height: 25px;" src="{{ asset('assets/img/logo.png') }}" alt="Logo"></a>
+                <a class="navbar-brand align-middle" href="{{ url('/home') }}"><img style="max-height: 25px;" src="{{ asset('assets/img/logo.png') }}" alt="Logo"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav gap-2 ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item" class="underline-anim"><a class="nav-link underline-anim underline-anim__blue" href="{{ url('/home')}}" style="color: var(--bs-blue)">Home</a></li>
-                        <li class="nav-item"><a class="nav-link underline-anim underline-anim__yellow" href="{{ url('/about')}}" style="color: var(--bs-yellow)">About</a></li>
-                        <li class="nav-item"><a class="nav-link underline-anim underline-anim__green" href="{{ url('/contact')}}" style="color: var(--bs-green)">Contact</a></li>
-                        <li class="nav-item"><a class="nav-link underline-anim underline-anim__red" href="{{ url('/faq')}}" style="color: var(--bs-red)">FAQ</a></li>
+                        <li class="nav-item"><a class="nav-link underline-anim underline-anim__blue fw-bold text-uppercase" href="{{ url('/home')}}" style="color: var(--bs-blue)">Home</a></li>
+                        <li class="nav-item"><a class="nav-link underline-anim underline-anim__yellow fw-bold text-uppercase" href="{{ url('/about')}}" style="color: var(--bs-yellow)">About</a></li>
+                        <li class="nav-item"><a class="nav-link underline-anim underline-anim__green fw-bold text-uppercase" href="{{ url('/contact')}}" style="color: var(--bs-green)">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link underline-anim underline-anim__red fw-bold text-uppercase" href="{{ url('/faq')}}" style="color: var(--bs-red)">FAQ</a></li>
                     </ul>
                 </div>
             </div>
